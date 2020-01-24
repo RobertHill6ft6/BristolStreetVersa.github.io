e410e237609c0ec9b6cf655eba7346b547680ed2
RobertHill6ft6 <59657640+RobertHill6ft6@users.noreply.github.com>
2020-01-24 12:33:09 +0000

    2nd website upload

---
+++ websiteScript.js
@@ -0,0 +1,44 @@
+//JavaScript for Bristol Street Motors Website Task.
+//Creator: Robert Hill
+//Date Edited: 24/01/2020
+-->
+function carColourChange(btnColour)
+{
+	var buttonId = btnColour;
+	if(btnColour == "black")
+	{
+		document.getElementById("carColour").src="./Black.png";
+	}
+		if(btnColour == "metalicious")
+	{
+		document.getElementById("carColour").src="./Metalicious.png";
+	}
+		if(btnColour == "magnetic")
+	{
+		document.getElementById("carColour").src="./Magnetic.png";
+	}
+		if(btnColour == "silver")
+	{
+		document.getElementById("carColour").src="./Silver.png";
+	}
+		if(btnColour == "guard")
+	{
+		document.getElementById("carColour").src="./Guard.png";
+	}
+		if(btnColour == "blazerblue")
+	{
+		document.getElementById("carColour").src="./BlazerBlue.png";
+	}
+		if(btnColour == "deepimpactblue")
+	{
+		document.getElementById("carColour").src="./DeepImpactBlue.png";
+	}
+		if(btnColour == "solar")
+	{
+		document.getElementById("carColour").src="./Solar.png";
+	}
+		if(btnColour == "red")
+	{
+		document.getElementById("carColour").src="./Red.png";
+	}
+}
